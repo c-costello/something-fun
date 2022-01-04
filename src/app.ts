6b13a42c4b8c1c735f00736f0f9b196b2f1597b8
Clarice Costello <claricostello@outlook.com>
2022-01-03 17:39:24 -0800

    linter changes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import express from "express";
+import * as express from "express";
 
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  // index.ts
+  const arr: string[] = ["one", "two", "three"];
+  if (arr.length == 0) {
+    throw new Error("Empty array");
+  }
+
+  res.send("Hello World!");
 });
+
 app.listen(port, () => {
-    return console.log(`server is listening on ${port}`);
+  return console.log(`server is listening on ${port}`);
 });
